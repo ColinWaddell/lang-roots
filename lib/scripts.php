2388f0d4f1c020c1c3a5d165fccdb7574a4b6dbb
Colin Waddell <mrcolin@gmail.com>
2014-02-19 22:15:24 +0000

    tidied up page title

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '63b23a17ad5556d84f2f89e9d19f865c');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a5c0d352cdb1341b0c3f554a1e7317d7');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
