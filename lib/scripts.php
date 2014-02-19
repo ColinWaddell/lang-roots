87532f251fbdf81f13d99571b8413b6b0b025003
Colin Waddell <mrcolin@gmail.com>
2014-02-19 22:10:09 +0000

    added lang's image to every page, for better or worse

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '30356bfccfe89144255656a2e7036630');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '63b23a17ad5556d84f2f89e9d19f865c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
