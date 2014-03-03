dc157526537860a9f66f21dff16ecf338f08e0ff
Colin Waddell <mrcolin@gmail.com>
2014-03-03 22:55:06 +0000

    updated style for sidebar list

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd04dedf27c5fc9207964a460636b0804');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '665945e66c7da9e8b464ac69ec61d76d');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
